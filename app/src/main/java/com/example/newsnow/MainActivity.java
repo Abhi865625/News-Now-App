29aa84e32f1585e50ca73fd030fbb131cecbb3d7
Abhishek Kumar Mehta <akmehta7252@gmail.com>
2023-10-01 09:13:29 +0530

    First Commit

---
+++ app/src/main/java/com/example/newsnow/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.newsnow;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7;
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+        searchView = findViewById(R.id.search_view);
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+        btn6 = findViewById(R.id.btn_6);
+        btn7 = findViewById(R.id.btn_7);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getNews("GENERAL",query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        setupRecyclerView();
+        getNews("GENERAL", null);
+    }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    void changeInProgess(boolean show){
+        if(show)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else progressIndicator.setVisibility(View.INVISIBLE);
+    }
+    //Method to get news
+
+    void getNews(String category, String query){
+        changeInProgess(true);
+        NewsApiClient newsApiClient = new NewsApiClient("1089d364059d4f3c8b7167cb745e385c");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("en")
+                        .category(category)
+                        .q(query)
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+
+                        runOnUiThread(()->{
+                            changeInProgess(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("GOT Failure", Objects.requireNonNull(throwable.getMessage()));
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String category = btn.getText().toString();
+        getNews(category, null);
+    }
+}
---
+++ app/src/main/java/com/example/newsnow/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.newsnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // wait for  3sec and go to next Activity
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
